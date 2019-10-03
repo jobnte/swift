ac0efc3f3c845ea45a4035d47c3c986f428d28b3
Jorge Bustamante <jorgelub12@gmail.com>
2019-10-03 11:37:35 -0500

    Add binary search extension using recursion

--- searches/binary_search.swift
+++ searches/binary_search.swift
@@ -23,3 +23,32 @@ print(binarySearch(list: [1,2,3,4,5], value: 3))
 print(binarySearch(list: [1,2,3,4,5], value: 4))
 print(binarySearch(list: [1,2,3,4,5], value: 5))
 print(binarySearch(list: [1,2,3,4,5], value: 6))
+
+
+// - MARK: Recursion Extension
+
+extension RandomAccessCollection where Element: Comparable {
+  func binarySearch(for value: Element, in range: Range<Index>? = nil) -> Index? {
+    let range = range ?? startIndex..<endIndex
+    guard range.lowerBound < range.upperBound else {
+      return nil
+    }
+
+    let size = distance(from: range.lowerBound, to: range.upperBound)
+    let middle = index(range.lowerBound, offsetBy: size / 2)
+
+    if self[middle] == value {
+      return middle
+    } else if self[middle] > value {
+      return binarySearch(for: value, in: range.lowerBound..<middle)
+    } else {
+      return binarySearch(for: value, in: index(after: middle)..<range.upperBound)
+    }
+  }
+}
+
+let list = [1, 2, 5, 10, 18, 25, 28, 28, 100, 115, 119]
+print(list.binarySearch(for: 0))
+print(list.binarySearch(for: 5))
+print(list.binarySearch(for: 25))
+print(list.binarySearch(for: 100))
